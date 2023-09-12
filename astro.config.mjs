58cfc94adfd713797ff4f81f8267de7a11eda49a
Antía <elyosis@proton.me>
2023-09-12 19:30:01 +0200

    Fixed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "sunnyside-elyosis.netlify.app",
+  site: "https://sunnyside-elyosis.netlify.app",
   base: "/"
 });
